7668db1a6456867816b56c5966d87cfaeaf66097
Ainhoa <Ainhoa.Genshin@gmail.com>
2024-09-24 01:22:40 +0200

    Added main page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from "vite";
+import {resolve} from "path";
 export default defineConfig({
     base: "/", // Replace with your GitHub repo name
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname,"index.html"),
+                about: resolve(__dirname,"src/pages/about.html")
+            }
+        }
+    }
 });
