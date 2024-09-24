3061acb2acb957f5ccfb9fc702f6d30d76ff6a6f
Ainhoa <Ainhoa.Genshin@gmail.com>
2024-09-24 23:10:03 +0200

    Updated some stuff + Added Chapter Pages

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from "vite";
 import {resolve} from "path";
 export default defineConfig({
     base: "/", // Replace with your GitHub repo name
+    root: "sites",
+    publicDir: "public",
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "sites/index.html"),
+                about: resolve(__dirname, "sites/about/index.html"),
+                chapter1: resolve(__dirname, "sites/chapter-1/index.html"),
+                chapter2: resolve(__dirname, "sites/chapter-2/index.html"),
+                chapter3: resolve(__dirname, "sites/chapter-3/index.html")
+            }
+        }
+    }
 });
