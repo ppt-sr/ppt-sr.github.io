8fc581ebe610d06ce7a028b7620f6145429079ac
Ainhoa <Ainhoa.Genshin@gmail.com>
2024-09-24 19:55:40 +0200

    Fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from "vite";
 import {resolve} from "path";
 export default defineConfig({
     base: "/", // Replace with your GitHub repo name
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname,"index.html"),
-                about: resolve(__dirname,"src/pages/about.html")
-            }
-        }
-    }
 });
